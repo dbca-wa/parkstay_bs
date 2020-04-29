6f9c48b0045a3ac63fe1a31df5e0fc7177cacf86
Jason Moore <jason.moore@dbca.wa.gov.au>
2020-04-29 11:05:41 +0800

    Covid Warnings

--- parkstay/views.py
+++ parkstay/views.py
@@ -230,6 +230,9 @@ class MakeBookingsView(TemplateView):
         booking.details['num_concession'] = form.cleaned_data.get('num_concession')
         booking.details['num_child'] = form.cleaned_data.get('num_child')
         booking.details['num_infant'] = form.cleaned_data.get('num_infant')
+        booking.details['toc'] = request.POST.get('toc',False)
+        booking.details['outsideregion'] = request.POST.get('outsideregion', False)
+
 
         # update vehicle registrations from form
         VEHICLE_CHOICES = {'0': 'vehicle', '1': 'concession', '2': 'motorbike'}
