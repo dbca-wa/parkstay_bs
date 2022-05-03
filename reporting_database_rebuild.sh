503949bcea2036d44a1c31660dc42f9380fcf633
Jason Moore <jmoore@austwa.com>
2022-05-03 12:17:15 +0800

    Update reporting_database_rebuild.sh

--- reporting_database_rebuild.sh
+++ reporting_database_rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  
+
 echo "Running database collection and merge into new database for reporting.";
 
 if [ $TEMPORARY_LEDGER_DATABASE ==  'ledger_prod' ]; then
@@ -8,20 +8,47 @@ if [ $TEMPORARY_LEDGER_DATABASE ==  'ledger_prod' ]; then
 fi
 
 if [ $TEMPORARY_LEDGER_DATABASE ==  'parkstayv2_prod' ]; then
-	echo "ERROR: parkstayv2_prod can not be a TEMPORARY Database";
-	exit;
+        echo "ERROR: parkstayv2_prod can not be a TEMPORARY Database";
+        exit;
 fi
 
 echo "Dump Core Ledger Production Tables";
-pg_dump --column-inserts "host=$PRODUCTION_LEDGER_HOST port=5432 dbname=$PRODUCTION_LEDGER_DATABASE user=$PRODUCTION_LEDGER_USERNAME password=$PRODUCTION_LEDGER_PASSWORD sslmode=require" -t 'accounts_emailuser' -t 'basket_basket' -t 'basket_line' -t 'order_order' -t 'order_line' -t 'address_country' -t 'payments_bpaytransaction' -t 'payments_bpointtransaction' -t 'payments_cashtransaction'  > /dbdumps/ledger_core_prod.sql
+PGPASSWORD="$PRODUCTION_LEDGER_PASSWORD" pg_dump -t 'accounts_emailuser' -t 'basket_basket' -t 'basket_line' -t 'order_order' -t 'order_line' -t 'address_country' -t 'payments_bpaytransaction' -t 'payments_bpointtransaction' -t 'payments_cashtransaction'  --file /dbdumps/ledger_core_prod.sql --format=custom --host $PRODUCTION_LEDGER_HOST --dbname $PRODUCTION_LEDGER_DATABASE --username $PRODUCTION_LEDGER_USERNAME
 
 echo "Dump Core Parkstay V1 Production Tables";
-pg_dump --column-inserts "host=$PRODUCTION_LEDGER_HOST port=5432 dbname=$PRODUCTION_LEDGER_DATABASE user=$PRODUCTION_LEDGER_USERNAME password=$PRODUCTION_LEDGER_PASSWORD sslmode=require" -t 'parkstay_*'  > /dbdumps/parkstayv1_core_prod.sql
+PGPASSWORD="$PRODUCTION_LEDGER_PASSWORD" pg_dump -t 'parkstay_*' --file /dbdumps/parkstayv1_core_prod.sql --format=custom --host $PRODUCTION_LEDGER_HOST --dbname $PRODUCTION_LEDGER_DATABASE --username $PRODUCTION_LEDGER_USERNAME
 
 echo "Dump Core Parkstay V2 Production Tables";
-pg_dump --column-inserts "host=$PRODUCTION_PARKSTAYV2_HOST port=5432 dbname=$PRODUCTION_PARKSTAYV2_DATABASE user=$PRODUCTION_PARKSTAYV2_USERNAME password=$PRODUCTION_PARKSTAYV2_PASSWORD sslmode=require" -t 'parkstay_*' > /dbdumps/parkstayv2_core_prod.sql
+PGPASSWORD="$PRODUCTION_PARKSTAYV2_PASSWORD" pg_dump -t 'parkstay_*' --file /dbdumps/parkstayv2_core_prod.sql --format=custom --host $PRODUCTION_PARKSTAYV2_HOST --dbname $PRODUCTION_PARKSTAYV2_DATABASE --username $PRODUCTION_PARKSTAYV2_USERNAME
 
+# DROP All TABLES IN DAILY DB
+for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "SELECT tablename FROM pg_tables where tablename not like 'pg\_%' and tablename not like 'sql\_%';" -t);
+  do
+  echo " drop table $I CASCADE; ";
+  psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "drop table $I CASCADE;" -t
+done
+#!/bin/bash
 
+echo "Running database collection and merge into new database for reporting.";
+
+if [ $TEMPORARY_LEDGER_DATABASE ==  'ledger_prod' ]; then
+        echo "ERROR: ledger_prod can not be a TEMPORARY Database";
+        exit;
+fi
+
+if [ $TEMPORARY_LEDGER_DATABASE ==  'parkstayv2_prod' ]; then
+        echo "ERROR: parkstayv2_prod can not be a TEMPORARY Database";
+        exit;
+fi
+
+echo "Dump Core Ledger Production Tables";
+PGPASSWORD="$PRODUCTION_LEDGER_PASSWORD" pg_dump -t 'accounts_emailuser' -t 'basket_basket' -t 'basket_line' -t 'order_order' -t 'order_line' -t 'address_country' -t 'payments_bpaytransaction' -t 'payments_bpointtransaction' -t 'payments_cashtransaction'  --file /dbdumps/ledger_core_prod.sql --format=custom --host $PRODUCTION_LEDGER_HOST --dbname $PRODUCTION_LEDGER_DATABASE --username $PRODUCTION_LEDGER_USERNAME
+
+echo "Dump Core Parkstay V1 Production Tables";
+PGPASSWORD="$PRODUCTION_LEDGER_PASSWORD" pg_dump -t 'parkstay_*' --file /dbdumps/parkstayv1_core_prod.sql --format=custom --host $PRODUCTION_LEDGER_HOST --dbname $PRODUCTION_LEDGER_DATABASE --username $PRODUCTION_LEDGER_USERNAME
+
+echo "Dump Core Parkstay V2 Production Tables";
+PGPASSWORD="$PRODUCTION_PARKSTAYV2_PASSWORD" pg_dump -t 'parkstay_*' --file /dbdumps/parkstayv2_core_prod.sql --format=custom --host $PRODUCTION_PARKSTAYV2_HOST --dbname $PRODUCTION_PARKSTAYV2_DATABASE --username $PRODUCTION_PARKSTAYV2_USERNAME
 
 # DROP All TABLES IN DAILY DB
 for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "SELECT tablename FROM pg_tables where tablename not like 'pg\_%' and tablename not like 'sql\_%';" -t);
@@ -30,13 +57,14 @@ for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_
   psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "drop table $I CASCADE;" -t
 done
 
-# IMPORT LEDGER PROD DATABASE INTO DAILY
+# IMPORT LEDGER PROD DATABASE INTO REPORTING 
 echo "Importing Ledger Core prod into reporting database";
-psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" < /dbdumps/ledger_core_prod.sql
+PGSSLMODE=require PGPASSWORD="$TEMPORARY_LEDGER_PASSWORD"  pg_restore  --host=$TEMPORARY_LEDGER_HOST --dbname=$TEMPORARY_LEDGER_DATABASE --username=$TEMPORARY_LEDGER_USERNAME /dbdumps/ledger_core_prod.sql
 
 # Parkstay V1
 echo "Importing Parkstay Core prod into reporting database";
-psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" < /dbdumps/parkstayv1_core_prod.sql
+PGSSLMODE=require PGPASSWORD="$TEMPORARY_LEDGER_PASSWORD"  pg_restore  --host=$TEMPORARY_LEDGER_HOST --dbname=$TEMPORARY_LEDGER_DATABASE --username=$TEMPORARY_LEDGER_USERNAME /dbdumps/parkstayv1_core_prod.sql
+
 
 echo "Renaming parkstay v1 tables";
 for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "SELECT tablename FROM pg_tables where tablename like 'parkstay\_%';" -t);
@@ -56,7 +84,7 @@ done
 
 # Parkstay V2
 echo "Importing Parkstay Core prod into reporting database";
-psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" < /dbdumps/parkstayv2_core_prod.sql
+PGSSLMODE=require PGPASSWORD="$TEMPORARY_LEDGER_PASSWORD"  pg_restore  --host=$TEMPORARY_LEDGER_HOST --dbname=$TEMPORARY_LEDGER_DATABASE --username=$TEMPORARY_LEDGER_USERNAME  /dbdumps/parkstayv2_core_prod.sql
 
 echo "Renaming parkstay v2 tables";
 for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "SELECT tablename FROM pg_tables where tablename like 'parkstay\_%';" -t);
@@ -84,4 +112,3 @@ done
 rm /dbdumps/ledger_core_prod.sql
 rm /dbdumps/parkstayv1_core_prod.sql
 rm /dbdumps/parkstayv2_core_prod.sql
-
