2c460962f260477a180b57a54a4cf0e4d65d1659
Jason Moore <jmoore@austwa.com>
2022-05-16 10:38:20 +0800

    Update reporting_database_rebuild.sh

--- reporting_database_rebuild.sh
+++ reporting_database_rebuild.sh
@@ -27,35 +27,6 @@ for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_
   echo " drop table $I CASCADE; ";
   psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "drop table $I CASCADE;" -t
 done
-#!/bin/bash
-
-echo "Running database collection and merge into new database for reporting.";
-
-if [ $TEMPORARY_LEDGER_DATABASE ==  'ledger_prod' ]; then
-        echo "ERROR: ledger_prod can not be a TEMPORARY Database";
-        exit;
-fi
-
-if [ $TEMPORARY_LEDGER_DATABASE ==  'parkstayv2_prod' ]; then
-        echo "ERROR: parkstayv2_prod can not be a TEMPORARY Database";
-        exit;
-fi
-
-echo "Dump Core Ledger Production Tables";
-PGPASSWORD="$PRODUCTION_LEDGER_PASSWORD" pg_dump -t 'accounts_emailuser' -t 'basket_basket' -t 'basket_line' -t 'order_order' -t 'order_line' -t 'address_country' -t 'payments_bpaytransaction' -t 'payments_bpointtransaction' -t 'payments_cashtransaction'  --file /dbdumps/ledger_core_prod.sql --format=custom --host $PRODUCTION_LEDGER_HOST --dbname $PRODUCTION_LEDGER_DATABASE --username $PRODUCTION_LEDGER_USERNAME
-
-echo "Dump Core Parkstay V1 Production Tables";
-PGPASSWORD="$PRODUCTION_LEDGER_PASSWORD" pg_dump -t 'parkstay_*' --file /dbdumps/parkstayv1_core_prod.sql --format=custom --host $PRODUCTION_LEDGER_HOST --dbname $PRODUCTION_LEDGER_DATABASE --username $PRODUCTION_LEDGER_USERNAME
-
-echo "Dump Core Parkstay V2 Production Tables";
-PGPASSWORD="$PRODUCTION_PARKSTAYV2_PASSWORD" pg_dump -t 'parkstay_*' --file /dbdumps/parkstayv2_core_prod.sql --format=custom --host $PRODUCTION_PARKSTAYV2_HOST --dbname $PRODUCTION_PARKSTAYV2_DATABASE --username $PRODUCTION_PARKSTAYV2_USERNAME
-
-# DROP All TABLES IN DAILY DB
-for I in $(psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "SELECT tablename FROM pg_tables where tablename not like 'pg\_%' and tablename not like 'sql\_%';" -t);
-  do
-  echo " drop table $I CASCADE; ";
-  psql "host=$TEMPORARY_LEDGER_HOST port=5432 dbname=$TEMPORARY_LEDGER_DATABASE user=$TEMPORARY_LEDGER_USERNAME password=$TEMPORARY_LEDGER_PASSWORD sslmode=require" -c "drop table $I CASCADE;" -t
-done
 
 # IMPORT LEDGER PROD DATABASE INTO REPORTING 
 echo "Importing Ledger Core prod into reporting database";
